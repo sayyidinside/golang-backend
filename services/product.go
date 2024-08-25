44666788e09f2203c51b7f4a7d60ba83d3a20b73
sayyidinside <sayyiddzulfikharhanif@gmail.com>
2024-08-25 13:17:55 +0700

    ➕ Create Add Product Service

--- services/product.go
+++ services/product.go
@@ -1,14 +1,52 @@
 package services
 
 import (
+	"golang/backend/database"
+	"golang/backend/dtos"
 	"golang/backend/helpers"
+	"golang/backend/models"
+
+	"github.com/google/uuid"
 )
 
-func FetchProducts() helpers.Response {
-	var response helpers.Response
+func InsertProduct(input *dtos.InputProductDTO) *dtos.Response {
+	// check existence of category id
+	var categoryCount int64
+	database.DBConn.Model(&models.ProductCategory{}).Where("id = ?", input.CategoryID).Count(&categoryCount)
+	if categoryCount == 0 {
+		return &dtos.Response{
+			Success: false,
+			Message: "Invalid data",
+			Error: helpers.ErrorResponse{
+				Field: "category_id",
+				Tag:   "not_found",
+				Value: input.CategoryID,
+			},
+		}
+	}
 
-	response.Success = true
-	response.Message = "Succesfully"
+	product := models.Product{
+		CategoryID:  uuid.MustParse(input.CategoryID),
+		Name:        input.Name,
+		Description: input.Description,
+	}
 
-	return response
+	if err := database.DBConn.Create(&product).Error; err != nil {
+		return &dtos.Response{
+			Success: false,
+			Message: "Failed creating data",
+			Error:   err.Error(),
+		}
+	}
+
+	return &dtos.Response{
+		Success: true,
+		Message: "Product successfully created",
+	}
+}
+
+func FetchProducts() *dtos.Response {
+	var response dtos.Response
+
+	return &response
 }
