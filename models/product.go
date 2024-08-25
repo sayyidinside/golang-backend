281ec36c22562e4af38112ff4117a931f7b0b285
sayyidinside <sayyiddzulfikharhanif@gmail.com>
2024-08-25 15:41:11 +0700

    ➕ Add Index To Product Name

--- models/product.go
+++ models/product.go
@@ -8,7 +8,7 @@ import (
 type Product struct {
 	ID          uuid.UUID       `json:"id" gorm:"primaryKey;type:char(36)"`
 	CategoryID  uuid.UUID       `json:"category_id" gorm:"index;type:char(36)"`
-	Name        string          `json:"name" gorm:"type:varchar(255)"`
+	Name        string          `json:"name" gorm:"index;type:varchar(255)"`
 	Description string          `json:"description" gorm:"type:text"`
 	Category    ProductCategory `json:"category" gorm:"foreignKey:CategoryID"`
 	gorm.Model
