b823167e54001136c9771c316d8e9bbfcdaccab6
sayyidinside <sayyiddzulfikharhanif@gmail.com>
2024-08-25 15:31:08 +0700

    ➕ Add CORS Implementation On All Routes

--- routers/router.go
+++ routers/router.go
@@ -2,13 +2,23 @@ package routers
 
 import (
 	"golang/backend/routers/apis"
+	"golang/backend/utils"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func SetupRouter(app *fiber.App) {
 	router := app.Group("")
-	apis.InitAPIsRoutes(router)
-	// InitV1Route
+
 	// Apply global middlewares here
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:  utils.GetENVWithDefault("ALLOW_ORIGINS", "*"),
+		AllowMethods:  utils.GetENVWithDefault("ALLOW_METHODS", "GET,POST,HEAD"),
+		AllowHeaders:  "*",
+		ExposeHeaders: "Content-Length",
+	}))
+
+	// Route list
+	apis.InitAPIsRoutes(router)
 }
