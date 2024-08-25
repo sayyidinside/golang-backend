cc0c7692afbe0b21ddcea333b47951c6b8c087d6
sayyidinside <sayyiddzulfikharhanif@gmail.com>
2024-08-25 13:18:10 +0700

    ➕ Create Add Product Controller

--- controllers/product.go
+++ controllers/product.go
@@ -1,10 +1,51 @@
 package controllers
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"golang/backend/dtos"
+	"golang/backend/helpers"
+	"golang/backend/services"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func GetProducts(c *fiber.Ctx) error {
-	return c.JSON(fiber.Map{
-		"success": true,
-		"data":    "test",
-	})
+	response := services.FetchProducts()
+
+	if response.Success {
+		c.Status(fiber.StatusOK)
+	} else {
+		c.Status(fiber.StatusInternalServerError)
+	}
+
+	return c.JSON(response)
+}
+
+func AddProduct(c *fiber.Ctx) error {
+	var input dtos.InputProductDTO
+	if err := c.BodyParser(&input); err != nil {
+		return c.
+			Status(fiber.StatusBadRequest).
+			JSON(dtos.Response{
+				Success: false,
+				Message: "Failed parsing user input",
+				Data:    nil,
+				Error:   err.Error(),
+			})
+	}
+
+	if res, err := helpers.ValidateInput(input); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(res)
+	}
+
+	response := services.InsertProduct(&input)
+
+	if response.Success {
+		c.Status(fiber.StatusCreated)
+	} else if response.Message == "Invalid data" {
+		c.Status(fiber.StatusBadRequest)
+	} else {
+		c.Status(fiber.StatusInternalServerError)
+	}
+
+	return c.JSON(response)
 }
